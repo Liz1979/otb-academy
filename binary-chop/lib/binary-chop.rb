92f10c8361c58d9be24ce5785e1718f3b4fb4583
Elizabeth Dalton <you@example.com>
2017-09-29 15:40:49 +0100

    final code

--- binary-chop/lib/binary-chop.rb
+++ binary-chop/lib/binary-chop.rb
@@ -2,22 +2,46 @@ class BinaryChop
   
     def initialize
         @array = []
-        @sorted_array = []
-    end
-   
-    def sorted_array
-        @array.sort! do |x, y|
-            x <=> y
-        end
     end
 
     def add_an_array(numbers)
-        array1 = numbers.split(/, /).map{|chr| chr.to_i}
+        array1 = numbers.split(//).map(&:to_i)
         array1.each do |x|
-            @array.push(x)
-            p @array
+            @array << x
+            @array.sort!
         end
     end
+
+    def middle_of_array()
+        if @array.length % 2 != 0
+             (@array.length + 1) / 2
+            else
+             @array.length / 2
+        end
+    end
+
     
-    
+    def find_a_number(n)
+        mid = middle_of_array
+        x = @array[mid]
+        l = @array.length-1
+        p @array
+        p x
+        if @array.include?(n)
+            while n != x 
+                if n > x
+                    @array.slice!(0, mid)
+                    mid = middle_of_array
+                    x = @array[mid]
+                elsif n < x
+                    @array.slice!(mid, l)
+                    mid = middle_of_array
+                    x = @array[mid]
+                end
+                puts "found #{n}"
+            end
+        else
+            "not found"
+        end
+    end
 end
--- binary-chop/spec/binary-chop_spec.rb
+++ binary-chop/spec/binary-chop_spec.rb
@@ -7,12 +7,9 @@ RSpec.describe "check that a binay chop works" do
   describe 'binary chop' do
     it "can add nubers to an array" do
       expected = 
-      expect( array.add_an_array(2, 3, 4, 5, 9, 8, 7, 6) ).to eq( expected )
+      expect( array.add_an_array("75108365") &&
+      array.find_a_number(2) ).to eq( expected )
     end
-
-      it "can sort an array" do
-        expected = 
-        expect( array.sorted_array() ).to eq( expected )
-      end    
+ 
   end
 end
