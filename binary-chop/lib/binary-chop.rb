b4278872b248d7967bac8da0a05292b4c3689a20
Elizabeth Dalton <you@example.com>
2017-09-29 16:40:33 +0100

    changes 2

--- binary-chop/lib/binary-chop.rb
+++ binary-chop/lib/binary-chop.rb
@@ -4,44 +4,36 @@ class BinaryChop
         @array = []
     end
 
-    def add_an_array(numbers)
-        array1 = numbers.split(//).map(&:to_i)
-        array1.each do |x|
-            @array << x
-            @array.sort!
-        end
-    end
-
     def middle_of_array()
         if @array.length % 2 != 0
-             (@array.length + 1) / 2
+             (@array.length - 1) / 2
             else
              @array.length / 2
         end
     end
 
-    
-    def find_a_number(n)
+    def find_a_number(numbers, n) # numbers = [1,3,5] // n = 5
+        @array = numbers
         mid = middle_of_array
         x = @array[mid]
-        l = @array.length-1
-        p @array
-        p x
+        l = @array.size - 1
         if @array.include?(n)
             while n != x 
-                if n > x
-                    @array.slice!(0, mid)
-                    mid = middle_of_array
-                    x = @array[mid]
-                elsif n < x
+                if n < x
                     @array.slice!(mid, l)
                     mid = middle_of_array
                     x = @array[mid]
+                elsif n > x
+                    @array.slice!(0, mid)
+                    mid = middle_of_array
+                    x = @array[mid]
                 end
+                p [@array,x, mid]        
                 puts "found #{n}"
             end
+            mid
         else
-            "not found"
+            -1
         end
     end
 end
--- binary-chop/spec/binary-chop_spec.rb
+++ binary-chop/spec/binary-chop_spec.rb
@@ -5,11 +5,28 @@ RSpec.describe "check that a binay chop works" do
   let(:array) { BinaryChop.new }
 
   describe 'binary chop' do
-    it "can add nubers to an array" do
-      expected = 
-      expect( array.add_an_array("75108365") &&
-      array.find_a_number(2) ).to eq( expected )
+    it 'implements binary search' do
+      # expect(array.find_a_number([], 3)).to eq(-1)
+      # expect(array.find_a_number([1], 3)).to eq(-1)
+      # expect(array.find_a_number([1], 1)).to eq(0)
+      # expect(array.find_a_number([1, 3, 5], 1)).to eq(0)
+      # expect(array.find_a_number([1, 3, 5], 3)).to eq(1)
+      
+      expect(array.find_a_number([1, 3, 5], 5)).to eq(2)
+
+      # expect(array.find_a_number([1, 3, 5], 0)).to eq(-1)
+      # expect(array.find_a_number([1, 3, 5], 2)).to eq(-1)
+      # expect(array.find_a_number([1, 3, 5], 4)).to eq(-1)
+      # expect(array.find_a_number([1, 3, 5], 6)).to eq(-1)
+      # expect(array.find_a_number([1, 3, 5, 7], 1)).to eq(0)
+      # expect(array.find_a_number([1, 3, 5, 7], 3)).to eq(1)
+      # expect(array.find_a_number([1, 3, 5, 7], 5)).to eq(2)
+      # expect(array.find_a_number([1, 3, 5, 7], 7)).to eq(3)
+      # expect(array.find_a_number([1, 3, 5, 7], 0)).to eq(-1)
+      # expect(array.find_a_number([1, 3, 5, 7], 2)).to eq(-1)
+      # expect(array.find_a_number([1, 3, 5, 7], 4)).to eq(-1)
+      # expect(array.find_a_number([1, 3, 5, 7], 6)).to eq(-1)
+      # expect(array.find_a_number([1, 3, 5, 7], 8)).to eq(-1)
     end
- 
   end
 end
